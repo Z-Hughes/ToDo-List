e1e3cb8780f7dde617506240e20712e675e5bb6b
Zach Hughes <zach.hughes79@gmail.com>
2022-04-26 16:01:25 -0400

    adding new items functionality

--- app.js
+++ app.js
@@ -75,16 +75,15 @@ app.get("/", function (req, res) {
 
 app.post("/", function (req, res) {
 
-  let item = req.body.newItem;
+  const itemName = req.body.newItem;
 
-  // checks to see which list the item was posted to
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
+    const item = new Item({
+      name: itemName
+    });
+
+    item.save();
     res.redirect("/");
-  }
+  
 });
 
 app.get("/work", function (req, res) {
